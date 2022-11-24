6f93ee5eeb112fcfa8489f636083a3a40c6bd295
Sham-Tze-Wah <shamtzewah@gmail.com>
2022-11-24 18:15:31 +0800

    First commit

---
+++ src/main/java/com/cinematicketsys/purchaseapi/PurchaseapiApplication.java
@@ -0,0 +1,13 @@
+package com.cinematicketsys.purchaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseapiApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/com/cinematicketsys/purchaseapi/PurchaseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinematicketsys.purchaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
